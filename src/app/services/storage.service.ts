0115155ac713b6532a3ffd20c1c59b1afa2d5070
cfmorales <cristian_moralese@hotmail.com>
2020-01-28 18:55:41 -0500

    Reportes Guardar

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,50 +1,60 @@
-import { Injectable } from '@angular/core';
-import { Plugins } from '@capacitor/core';
-const { Storage } = Plugins;
+import {Injectable} from '@angular/core';
+import {Plugins} from '@capacitor/core';
+
+const {Storage} = Plugins;
+
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class StorageService {
-  constructor() {}
-
-  // Store the value
-  async store(storageKey: string, value: any) {
-    const encryptedValue = btoa(escape(JSON.stringify(value)));
-    await Storage.set({
-      key: storageKey,
-      value: encryptedValue
-    });
-  }
-
-  // Get the value
-  async get(storageKey: string) {
-    const ret = await Storage.get({ key: storageKey });
-    if (ret.value) {
-      return JSON.parse(unescape(atob(ret.value)));
-    } else {
-      return false;
+    constructor() {
     }
-  }
 
-  // JSON "get" example
-  async getObject() {
-    const ret = await Storage.get({ key: 'user' });
-    const user = JSON.parse(ret.value);
-  }
+    // Store the value
+    async store(storageKey: string, value: any) {
+        const encryptedValue = btoa(escape(JSON.stringify(value)));
+        await Storage.set({
+            key: storageKey,
+            value: encryptedValue
+        });
+    }
 
-  async setItem() {
-    await Storage.set({
-      key: 'name',
-      value: 'Max'
-    });
-  }
+    async storeValues(storageKey: string, value: any) {
+        await Storage.set({
+            key: storageKey,
+            value: JSON.stringify(value)
+        });
+    }
 
-  async keys() {
-    const keys = await Storage.keys();
-    console.log('Got keys: ', keys);
-  }
+    // Get the value
+    async get(storageKey: string) {
+        const ret = await Storage.get({key: storageKey});
+        if (ret.value) {
+            return JSON.parse(unescape(atob(ret.value)));
+        } else {
+            return false;
+        }
+    }
 
-  async clear() {
-    await Storage.clear();
-  }
+    // JSON "get" example
+    async getObject() {
+        const ret = await Storage.get({key: 'user'});
+        const user = JSON.parse(ret.value);
+    }
+
+    async setItem() {
+        await Storage.set({
+            key: 'name',
+            value: 'Max'
+        });
+    }
+
+    async keys() {
+        const keys = await Storage.keys();
+        console.log('Got keys: ', keys);
+    }
+
+    async clear() {
+        await Storage.clear();
+    }
 }
