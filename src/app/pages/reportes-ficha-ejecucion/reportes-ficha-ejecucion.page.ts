49db4ee4fdede07b97b1c7ef99d28eff87dba0f3
cfmorales <cristian_moralese@hotmail.com>
2020-01-30 00:47:35 -0500

    Generar pdf

--- src/app/pages/reportes-ficha-ejecucion/reportes-ficha-ejecucion.page.ts
+++ src/app/pages/reportes-ficha-ejecucion/reportes-ficha-ejecucion.page.ts
@@ -13,6 +13,7 @@ import {File} from '@ionic-native/file/ngx';
 import {FileOpener} from '@ionic-native/file-opener/ngx';
 import {Platform} from '@ionic/angular';
 
+
 @Component({
     selector: 'app-reportes-ficha-ejecucion',
     templateUrl: './reportes-ficha-ejecucion.page.html',
@@ -35,13 +36,13 @@ export class ReportesFichaEjecucionPage implements OnInit {
     public verifMan = [
         {val: 'Limpieza', isChecked: false, caso: '1'},
         {val: 'Partes Eléctricas', isChecked: false, caso: '2'},
-        {val: 'Partes Mecánicas', isChecked: false, caso: '3'},
+        {val: 'Partes Mecánicas', isChecked: true, caso: '3'},
         {val: 'Partes Neumaticas', isChecked: false, caso: '4'},
         {val: 'Tuberias', isChecked: false, caso: '5'},
-        {val: 'Estructura', isChecked: false, caso: '6'},
+        {val: 'Estructura', isChecked: true, caso: '6'},
         {val: 'Tiempo de prueba', isChecked: false, caso: '7'},
         {val: 'Horómetro', isChecked: false, caso: '8'},
-        {val: 'Herramientas', isChecked: false, caso: '9'}
+        {val: 'Herramientas', isChecked: true, caso: '9'}
     ];
     public especialidad = [
         {tipo: 'Mecánica', isChecked: false, val: '1'},
@@ -100,6 +101,51 @@ export class ReportesFichaEjecucionPage implements OnInit {
         }
     }
 
+    private comprobarEPM(caso, arrayCaso) {
+        switch (caso) {
+            case 1:
+                switch (arrayCaso) {
+                    case '1':
+                        return 'Mecánica';
+                        break;
+                    case '2':
+                        return 'Eléctrica';
+                        break;
+                    case '3':
+                        return 'Control';
+                        break;
+                }
+                break;
+            case 2:
+                switch (arrayCaso) {
+                    case '1':
+                        return 'Emergente';
+                        break;
+                    case '2':
+                        return 'Urgente';
+                        break;
+                    case '3':
+                        return 'Programable';
+                        break;
+                }
+                break;
+            case 3:
+                switch (arrayCaso) {
+                    case '1':
+                        return 'Correctivo';
+                        break;
+                    case '2':
+                        return 'Preventivo';
+                        break;
+                    case '3':
+                        return 'Horas';
+                        break;
+                }
+                break;
+        }
+    }
+
+
     public especialidadEdit(): void {
         this.especialidad.forEach(item => {
             if (item.val === this.orden.especialidad) {
@@ -132,7 +178,7 @@ export class ReportesFichaEjecucionPage implements OnInit {
     }
 
     descargarPDF(ord) {
-        console.log(ord);
+
         const docDefinition = {
             content: [
                 {text: 'Datos del trabajo.', style: 'header'},
@@ -169,34 +215,78 @@ export class ReportesFichaEjecucionPage implements OnInit {
                             text: ['Recibido por: ' + ord.recibido_sintomas + '\n'],
                         },
                         {
-                            text: ['Fecha: ' + ord.fecha_sintomas + '\n',],
+                            text: ['Fecha: ' + ord.fecha_sintomas + '\n'],
                         }
                     ]
                 },
-                {text: 'REMINDER', style: 'header'},
-                {text: new Date().toTimeString(), alignment: 'right'},
+                {
+                    style: 'header',
+                    columns: [
+                        {
+                            text: ['Especialidad'],
+                        },
+                        {
+                            text: ['Prioridad'],
+                        },
+                        {
+                            text: ['Mantenimiento'],
+                        }
+                    ]
+                },
+                {
+                    alignment: 'justify',
+                    columns: [
 
-                {text: 'From', style: 'subheader'},
-                {text: 'this.letterObj.from'},
-
-                {text: 'To', style: 'subheader'},
-                'this.letterObj.to',
-
-                {text: 'this.letterObj.text', style: 'story', margin: [0, 20, 0, 20]},
+                        {
+                            text: [this.comprobarEPM(1, ord.especialidad) + ''],
+                        },
+                        {
+                            text: [this.comprobarEPM(2, ord.prioridad) + ''],
+                        },
+                        {
+                            text: [this.comprobarEPM(3, ord.mantenimiento) + ''],
+                        }
+                    ]
+                },
+                {text: 'Detalla el trabajo realizado.', style: 'header'},
+                {text: ord.detalle_trabajo, alignment: 'left'},
+                {text: 'Responsable: ' + ord.user_id_fk, alignment: 'left'},
+                {text: 'Control post mantenimiento.', style: 'header'},
 
                 {
-                    ul: [
-                        'Bacon',
-                        'Rips',
-                        'BBQ',
+                    alignment: 'justify',
+                    columns: [
+                        {
+                            text: ['Funciona eficientemente: ' + ord.contro_post.funciona_eficiente.caso + '\n',
+                                'Se eliminó el daño: ' + ord.contro_post.elim_danio.caso + '\n',
+                                'Quedó limpia el area: ' + ord.contro_post.area_limpia.caso + '\n',
+                                'Libre de cuerpos extraños: ' + ord.contro_post.libre_cuerpos.caso + '\n',
+                            ],
+                        },
+                        {
+                            text: [ord.contro_post.funciona_eficiente.descrip + '\n',
+                                ord.contro_post.elim_danio.descrip + '\n',
+                                ord.contro_post.area_limpia.descrip + '\n',
+                                ord.contro_post.libre_cuerpos.descrip + '\n',
+                            ],
+                        }
                     ]
-                }
+                },
+                {text: 'Fecha y hora: ' + ord.contro_post.fecha_hora, alignment: 'left'},
+                {text: 'Nombre Usuario: ' + ord.contro_post.nombre_usuario, alignment: 'left'},
+                {text: 'Tiempo total parada. ' + ord.contro_post.tiempo_parada, alignment: 'left'},
+                {text: 'Firma Mtto: ' + ord.contro_post.firma_mtto, alignment: 'left'},
+                {text: 'Observaciones:', style: 'header'},
+                {text: '' + ord.observaciones, alignment: 'left'},
+
+
             ],
             styles: {
                 header: {
                     fontSize: 18,
                     bold: true,
                     alignment: 'center',
+                    margin: [0, 20, 0, 20]
                 },
                 subheader: {
                     fontSize: 14,
